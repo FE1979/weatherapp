ad15238007d4d258d97d4625e88499fc80dae021
Roman <ronin911@gmail.com>
2018-11-20 13:56:08 +0200

    reads Realfeel and Condition. Encoding Conditions and </b in RF not solved

--- weatherapp.py
+++ weatherapp.py
@@ -35,7 +35,9 @@ end = ACCU_PAGE.find(",", start + 1)
 ACCU_Data['RealFeel'] = ACCU_PAGE[start + len(ACCU_RealFeel_tag):end-3]
 
 print('\nПоточна погода за даними Accuweather:\n')
-print(f"Температура {ACCU_Data['Temperature']:.0f} {unescape('&deg')}C, відчувається як {ACCU_Data['RealFeel']}, на небі - {ACCU_Data['Condition']}\n")
+print(f"Температура {ACCU_Data['Temperature']:.0f} {unescape('&deg')}C,"
+     f"відчувається як {ACCU_Data['RealFeel']} {unescape('&deg')}C,"
+     f" на небі - {ACCU_Data['Condition']}\n")
 
 a = input('RP5')
 
@@ -47,7 +49,8 @@ RP5_REQUEST = Request(RP5_URL, headers = RP5_HEAD)
 RP5_PAGE = urlopen(RP5_REQUEST).read()
 RP5_PAGE = str(RP5_PAGE)
 
-RP5_Condition_tag = ''
+RP5_Condition_tags = {'1': "ftab_1_content",
+                        '2': "<b>"}
 RP5_TEMP_tag = '\"t_0\" style=\"display: block;\">'
 RP5_RealFeel_tag = ''
 RP5_Data = {}
@@ -57,8 +60,18 @@ start = RP5_PAGE.find(RP5_TEMP_tag)
 end = RP5_PAGE.find("<", start + 1)
 RP5_temp = RP5_PAGE[start + len(RP5_TEMP_tag):end-1]
 RP5_Data['Temperature'] = RP5_temp
-RP5_Data['RealFeel'] = RP5_temp
-RP5_Data['Condition'] = RP5_temp
+
+"""Condition"""
+start = RP5_PAGE.find(RP5_Condition_tags['1'])
+start = RP5_PAGE.find(RP5_Condition_tags['2'], start)
+end = RP5_PAGE.find("</b>", start)
+RP5_Data['Condition'] = RP5_PAGE[start + len(RP5_Condition_tags['2']):end-1]
+
+"""RealFeel"""
+start = RP5_PAGE.find("f_temperature", end)
+start = RP5_PAGE.find("<div class=\"t_0\">", start)
+end = RP5_PAGE.find("</div>", start)
+RP5_Data['RealFeel'] = RP5_PAGE[start + len("<div class=\"t_0\">"):end-1]
 
 print('\nПоточна погода за даними RP5:\n')
 print(f"Температура {unescape(RP5_Data['Temperature'])}C, відчувається як {RP5_Data['RealFeel']}, на небі - {RP5_Data['Condition']}\n")
