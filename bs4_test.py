7cf348157c0aa02a94c130ef7158e627ac2fd566
Roman <ronin911@gmail.com>
2018-12-05 19:09:43 +0200

    Hourly forecast in bs4_test. ToDo: move it to weatherapp and delete bs4_test.py

--- bs4_test.py
+++ bs4_test.py
@@ -26,6 +26,24 @@ RealFeel = RealFeel[10:] #remove word 'RealFeel' from it
 
 print(temperature, RealFeel, Condition)
 
+"""Hourly forecast ACCUWEATHER"""
+URL = "https://www.accuweather.com/uk/ua/kyiv/324505/hourly-weather-forecast/324505"
+
+page = get_raw_page(URL)
+soup = BeautifulSoup(page, 'html.parser')
+
+hourly_data = soup.find('div', class_='hourly-table overview-hourly') #find hourly forecast table
+hourly_data = hourly_data.select('tbody tr span') #extract only <span> with data
+
+i=0
+hourly_temperature = []
+while i<8: #move 8 times (8 hours forecast)
+    hourly_temperature.append(int(list(hourly_data)[i].string[:1]))
+    i += 1
+    pass
+print(hourly_temperature)
+print(f"""Max t {max(hourly_temperature)}, min {min(hourly_temperature)}
+        average {sum(hourly_temperature)/len(hourly_temperature)}""")
 
 """
 for item in soup.select('div'):
