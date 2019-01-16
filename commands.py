fe0136c0761b5a8438072f0c536f099f676136b1
Roman <ronin911@gmail.com>
2019-01-16 21:51:10 +0200

    Added option to configure user-defined provider

--- commands.py
+++ commands.py
@@ -49,21 +49,31 @@ class Configure(Command):
 
     def run(self):
         print(self.app.remaining_args)
-        self.set_options()
+
+        if self.app.remaining_args[0]:
+            provider_title = self.app.remaining_args[0]
+        if provider_title in self.app.providers._providers: #if first argument is provider title
+            self.set_options(provider_title)
+            #self.set_location(provider_title)
+        else: #if no provider chosen do it for all
+            for provider_title in self.app.providers._providers:
+                self.set_options(provider_title)
+            #    self.set_location(provider_title)
         print('Bye-bye!')
 
-    def set_options(self):
-        for item in self.app.providers._providers:
-            for key in config.PROVIDERS_CONF[item]:
-                if key == 'Next_day':
-                    choice = input(f"{item}, show next day forecast? Y/N\n")
-                    if choice.lower() == 'y':
-                        config.PROVIDERS_CONF[item]['Next_day'] = True
-                    elif choice.lower() == 'n':
-                        config.PROVIDERS_CONF[item]['Next_day'] = False
-                elif key == 'Next_hours':
-                    choice = input(f"{item}, show next hours forecast? Y/N\n")
-                    if choice.lower() == 'y':
-                        config.PROVIDERS_CONF[item]['Next_hours'] = True
-                    if choice.lower() == 'n':
-                        config.PROVIDERS_CONF[item]['Next_hours'] = False
+    def set_options(self, provider_title):
+        """ sets options for provider """
+
+        for key in config.PROVIDERS_CONF[provider_title]:
+            if key == 'Next_day':
+                choice = input(f"{provider_title}, show next day forecast? Y/N\n")
+                if choice.lower() == 'y':
+                    config.PROVIDERS_CONF[provider_title]['Next_day'] = True
+                elif choice.lower() == 'n':
+                    config.PROVIDERS_CONF[provider_title]['Next_day'] = False
+            elif key == 'Next_hours':
+                choice = input(f"{provider_title}, show next hours forecast? Y/N\n")
+                if choice.lower() == 'y':
+                    config.PROVIDERS_CONF[provider_title]['Next_hours'] = True
+                if choice.lower() == 'n':
+                    config.PROVIDERS_CONF[provider_title]['Next_hours'] = False
