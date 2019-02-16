7b72adb45eef6fe2e48a022beec3c6fd267b5da2
Roman <ronin911@gmail.com>
2019-02-16 13:30:35 +0200

    Commenting and styling providermanager.py

--- weatherapp.core/weatherapp/core/managers/providermanager.py
+++ weatherapp.core/weatherapp/core/managers/providermanager.py
@@ -3,32 +3,40 @@
 from abstract.providers import AccuProvider, RP5_Provider, SinoptikProvider
 import abstract.abstract
 
+
 class ProviderManager(abstract.abstract.Manager):
-    """ Imports providers from providers.py
-    """
+    """ Container for providers """
 
     def __init__(self):
         self._providers = {}
         self._load_providers()
 
     def _load_providers(self):
-        """ Loads existing providers
-        """
+        """ Loads existing providers """
 
         for provider in [AccuProvider, RP5_Provider, SinoptikProvider]:
             self.add(provider.title, provider)
 
     def add(self, name, provider):
         """ Add provider
+            :param name: provider title
+            :param provider: provider instance
         """
+
         self._providers[name] = provider
 
     def get(self, name):
         """ Get provider by name
+            :param name: provider name
+            :return: provider instance
         """
 
         return self._providers.get(name, None)
 
     def get_list(self):
-        """ gets list of providers """
+        """ Gets list of providers
+            :return: providers list
+            :rtype: list
+        """
+
         return self._providers.keys()
