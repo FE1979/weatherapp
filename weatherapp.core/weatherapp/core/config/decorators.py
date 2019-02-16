16a14aa29adfe5fe8fb3b83bd07d94d5dbd82a85
Roman <ronin911@gmail.com>
2019-02-16 12:45:48 +0200

    Styling decorators.py

--- weatherapp.core/weatherapp/core/config/decorators.py
+++ weatherapp.core/weatherapp/core/config/decorators.py
@@ -3,11 +3,12 @@
 import time
 
 """ Globals """
-
 FUNCTIONS_CACHE = {}
+
 
 def pause_moment(func, *args, **kwargs):
     """ Pause for 1 second """
+
     def wrapper(*args, **kwargs):
         print('One moment, please...\n')
         time.sleep(1)
@@ -15,8 +16,10 @@ def pause_moment(func, *args, **kwargs):
         return res
     return wrapper
 
+
 def pause(func, *args, **kwargs):
     """ Pause for user defined time in seconds """
+
     def wrapper(*args, **kwargs):
         try:
             pause_time = int(input("Enter time in second to pause\n"))
@@ -30,19 +33,24 @@ def pause(func, *args, **kwargs):
         return res
     return wrapper
 
+
 def run_time(func, *args, **kwargs):
     """ Displays run time of a function """
+
     def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
         res = func(*args, *kwargs)
         run_time = time.perf_counter() - start_time
-        print(f'Function {func.__name__} from {func.__module__}  ran for {run_time:.5f} seconds')
+        print(f'Function {func.__name__} from {func.__module__} ' +
+            'ran for {run_time:.5f} seconds')
 
         return res
     return wrapper
 
+
 def show_variables(func, *args, **kwargs):
     """ Shows functions variables """
+
     def wrapper(*args, **kwargs):
         if args is not None:
             print(f'\nFunction {func.__name__} got positional vars:')
@@ -58,8 +66,9 @@ def show_variables(func, *args, **kwargs):
     return wrapper
 
 
-def times_called(func, count = 0, *args, **kwargs):
+def times_called(func, count=0, *args, **kwargs):
     """ Counts function calling times """
+
     def wrapper(*args, **kwargs):
         res = func(*args, **kwargs)
         return res
@@ -68,13 +77,16 @@ def times_called(func, count = 0, *args, **kwargs):
 
     return wrapper
 
-def function_cache(func, count = 0, *args, **kwargs):
+
+def function_cache(func, count=0, *args, **kwargs):
     """ Saves to global FUNCTIONS_CACHE result of called function """
+
     def wrapper(*args, **kwargs):
         res = func(*args, *kwargs)
         FUNCTIONS_CACHE[f"{func.__name__}_{count}"] = res
         return res
     return wrapper
+
 
 def show_loading(func, *args, **kwargs):
     def wrapper(*args, **kwargs):
