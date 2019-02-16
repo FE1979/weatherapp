9877fa5989ad1c5dcef2ad36b9a99485b4fd80ae
Roman <ronin911@gmail.com>
2019-02-16 10:55:02 +0200

    Comments and styling config.py

--- weatherapp.core/weatherapp/core/config/config.py
+++ weatherapp.core/weatherapp/core/config/config.py
@@ -1,4 +1,13 @@
-""" Configuration module for weatherapp.py
+""" Configuration module for application
+
+    Saves options into next files, located in core directory:
+    weather_config.ini:   # application configuration
+    providers_conf.json:  # providers configuration
+
+    Defaults:
+    WEATHER_PROVIDERS:    # providers defaults; cache path; display type
+    PROVIDERS_CONF:       # providers web parsing options
+
 """
 
 from urllib.parse import quote, unquote
@@ -9,20 +18,23 @@ import json
 import config.decorators
 
 """ Define global params """
-#WEATHER_PROVIDERS used as default values for first start or settings container
+# Defaults
 WEATHER_PROVIDERS = {
 'App': {
         'Cache_path': str(pathlib.Path.cwd() / 'Cache'),
         'Display': 'table'
         },
 'Accuweather': {'Title': 'Accuweather',
-        'URL': "https://www.accuweather.com/uk/ua/kyiv/324505/weather-forecast/324505",
-        'URL_hourly': "https://www.accuweather.com/uk/ua/kyiv/324505/hourly-weather-forecast/324505",
-        'URL_next_day': "https://www.accuweather.com/uk/ua/kyiv/324505/daily-weather-forecast/324505?day=2",
+        'URL': "https://www.accuweather.com" +
+                "/uk/ua/kyiv/324505/weather-forecast/324505",
+        'URL_hourly': "https://www.accuweather.com" +
+                "/uk/ua/kyiv/324505/hourly-weather-forecast/324505",
+        'URL_next_day': "https://www.accuweather.com" +
+                "/uk/ua/kyiv/324505/daily-weather-forecast/324505?day=2",
         'Location': 'Київ',
         'URL_locations': "https://www.accuweather.com/uk/browse-locations",
         'Caching_time': 60
-        },
+                },
 'RP5': {'Title': 'RP5',
         'URL': "http://rp5.ua/" + quote("Погода_в_Києві"),
         'Location': 'Київ',
@@ -36,6 +48,7 @@ WEATHER_PROVIDERS = {
         'Caching_time': 60
         }
 }
+# Defaults
 PROVIDERS_CONF = {
                 'Accuweather': {'Show': True,
                                 'Next_day': False,
@@ -58,19 +71,22 @@ CONFIG.optionxform = str
 CONFIG_PATH = 'weather_config.ini'
 PROVIDERS_CONF_PATH = 'providers_conf.json'
 
-""" End of global params """
-
-""" Config settings and fuctions """
 
 def restore_providers_conf():
-    """ restores providers configuration file """
+    """ Restores providers configuration file """
+
     path_providers = pathlib.Path(PROVIDERS_CONF_PATH)
 
     with open(path_providers, 'w') as conf_file:
         json.dump(PROVIDERS_CONF, conf_file)
 
+
 def load_providers_conf():
-    """ loads providers configuration """
+    """ Loads providers configuration
+        :return: provider conf loaded from providers_conf.json
+        :rtype: dictionary
+    """
+
     path_providers = pathlib.Path(PROVIDERS_CONF_PATH)
 
     with open(path_providers, 'r') as conf_file:
@@ -78,17 +94,22 @@ def load_providers_conf():
 
     return providers_configuration
 
+
 def write_providers_conf():
-    """ writes providers configuration """
+    """ Saves to file providers configuration """
+
     path_providers = pathlib.Path(PROVIDERS_CONF_PATH)
 
     with open(path_providers, 'w') as conf_file:
         json.dump(PROVIDERS_CONF, conf_file)
 
+
 def initiate_providers_conf():
-    """ checks if configuration file existed
+    """ Checks if configuration file existed
         if no file creates one with default conf
         Loads configuration from a file at the end
+        :return: provider conf loaded from providers_conf.json
+        :rtype: dictionary
     """
     path_providers = pathlib.Path(PROVIDERS_CONF_PATH)
 
@@ -99,8 +120,13 @@ def initiate_providers_conf():
 
     return providers_configuration
 
+
 def write_config(config):
-    """ writes WEATHER_PROVIDERS attributes to config """
+    """ Writes WEATHER_PROVIDERS attributes to config
+        :param config: ConfigParser instance
+        :return: config
+        :rtype: ConfigParser instance
+    """
 
     for item in WEATHER_PROVIDERS:
         config[item] = {}
@@ -109,58 +135,75 @@ def write_config(config):
 
     return config
 
+
 def save_config(config):
-    """ Saves config to file with current values """
+    """ Saves config to file with current values
+        :param config: ConfigParser instance
+    """
 
     config = write_config(config)
+    path = pathlib.Path(CONFIG_PATH)
 
-    with open('weather_config.ini', 'w') as f:
+    with open(path, 'w') as f:
         config.write(f)
+
 
 def load_config(config):
     """ Loads configuration to WEATHER_PROVIDERS
+        :param config: ConfigParser instance
+        :return: providers settings
+        :rtype: dictionary
     """
+
     weather_providers = {}
 
     config.read(CONFIG_PATH)
 
-    #load configuration to the weather_providers dict
-    #if no entry - pass. Check config file with is_valid()
+    # load configuration to the weather_providers dict
+    # if no entry - pass. Check config file with is_valid()
     try:
         for item in config:
             weather_providers[item] = {}
             for key in config[item]:
                 if key == 'Caching_time':
-                    weather_providers[item]['Caching_time'] = int(config[item][key])
-                elif key == 'Location': #if cyrillic titles than do not urllib.quote
+                    weather_providers[item]['Caching_time'] = \
+                        int(config[item][key])
+                elif key == 'Location':
                     weather_providers[item][key] = config[item][key]
-                else: #if URL
-                    weather_providers[item][key] = quote(config[item][key], safe="""://?=\ """)
+                # if key is URL apply 'urlib.quote'
+                else:
+                    weather_providers[item][key] = \
+                        quote(config[item][key], safe="""://?=\ """)
     except ValueError:
         pass
 
     return weather_providers
 
+
 def restore_config(config):
-    """ Restores config with defaults """
-    """
-    config['Accuweather'] = {}
-    config['Sinoptik'] = {}
-    config['RP5'] = {}
+    """ Restores config with defaults
+        :param config: ConfigParser instance
+        :return: config
+        :rtype: ConfigParser instance
     """
 
     config = write_config(config)
 
     return config
 
+
 def initiate_config(config):
     """ Initiates config
         Sets weather_providers and other conf variables
+        :param config: ConfigParser instance
+        :return: configuration and providers settings dictionary
+        :rtype: tuple
     """
+
     weather_providers = {}
     path = pathlib.Path(CONFIG_PATH)
 
-    if not path.exists(): #create new config file with defaults
+    if not path.exists():  # create new config file with defaults
         config = restore_config(config)
         save_config(config)
 
@@ -168,11 +211,14 @@ def initiate_config(config):
 
     return config, weather_providers
 
+
 def set_config(title, variables, weather_providers):
     """ Sets new config variables
-        title - title of weather provider
-        variables - provider variables
-        weather_providers - output
+        :param title: weather provider title
+        :param variables: provider settings
+        :param weather_providers: providers settings dictionary
+        :return: providers settings
+        :rtype: dictionary
     """
 
     if title == 'Accuweather':
@@ -187,32 +233,37 @@ def set_config(title, variables, weather_providers):
 
     return weather_providers
 
+
 def is_valid():
     """ Checks config file for existing values in config file
-        Returns False if config file is broken
         Should be run after initiate_config() (loaded file)
+        :return: False if config file is broken
+        :rtype: boolean
     """
+
     valid_config = True
 
-    #check if values in config exists
+    # check if values in config exists
     for item in WEATHER_PROVIDERS:
-        if item !='':
+        if item != '':
             for key in WEATHER_PROVIDERS[item]:
                 if WEATHER_PROVIDERS[item][key] == "" or \
-                    WEATHER_PROVIDERS[item][key] == None:
-                    valid_config = False
+                    WEATHER_PROVIDERS[item][key] is None:
+                        valid_config = False
         else:
             valid_config = False
 
     for item in PROVIDERS_CONF:
-        if item !='':
+        if item != '':
             for key in PROVIDERS_CONF[item]:
                 if PROVIDERS_CONF[item][key] == "" or \
-                    PROVIDERS_CONF[item][key] == None:
-                    valid_config = False
+                    PROVIDERS_CONF[item][key] is None:
+                        valid_config = False
         else:
             valid_config = False
+
     return valid_config
+
 
 CONFIG, WEATHER_PROVIDERS = initiate_config(CONFIG)
 PROVIDERS_CONF = initiate_providers_conf()
