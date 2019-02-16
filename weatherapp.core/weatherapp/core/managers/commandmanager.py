f9754aed9c0811e5c6441e96732829b5720fa27a
Roman <ronin911@gmail.com>
2019-02-16 13:16:34 +0200

    UpD: Commenting and styling commandmanager.py

--- weatherapp.core/weatherapp/core/managers/commandmanager.py
+++ weatherapp.core/weatherapp/core/managers/commandmanager.py
@@ -7,11 +7,9 @@ import abstract.abstract
 class CommandManager(abstract.abstract.Manager):
     """ Container for commands """
 
-
     def __init__(self):
         self.commands = {}
         self._load_commands()
-
 
     def __iter__(self):
         """ Let make it iterable
@@ -21,19 +19,16 @@ class CommandManager(abstract.abstract.Manager):
         for key, value in self.commands.items():
             yield key, value
 
-
     def _load_commands(self):
         """ Loads commands from commands.py """
 
         for item in [ConfigureApp, Configure]:
             self.commands[item.name] = item
 
-
     def add(self, name, command):
         """ Add command """
 
         self.commands[name] = command
-
 
     def get(self, name):
         """ Get provider by name
