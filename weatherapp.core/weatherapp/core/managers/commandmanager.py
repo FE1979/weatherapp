29f3a068f64f174be51bae77e94bcfc8cabb43d6
Roman <ronin911@gmail.com>
2019-02-16 12:49:40 +0200

    Commenting and styling commandmanager.py

--- weatherapp.core/weatherapp/core/managers/commandmanager.py
+++ weatherapp.core/weatherapp/core/managers/commandmanager.py
@@ -3,16 +3,24 @@
 from abstract.commands import ConfigureApp, Configure
 import abstract.abstract
 
+
 class CommandManager(abstract.abstract.Manager):
     """ Container for commands """
+
 
     def __init__(self):
         self.commands = {}
         self._load_commands()
 
+
     def __iter__(self):
+        """ Let make it iterable
+            :return: command name and instance
+            :rtype: tuple
+        """
         for key, value in self.commands.items():
             yield key, value
+
 
     def _load_commands(self):
         """ Loads commands from commands.py """
@@ -20,12 +28,17 @@ class CommandManager(abstract.abstract.Manager):
         for item in [ConfigureApp, Configure]:
             self.commands[item.name] = item
 
+
     def add(self, name, command):
         """ Add command """
+
         self.commands[name] = command
+
 
     def get(self, name):
         """ Get provider by name
+            :return: Command instance
+            :rtype: command
         """
 
         return self.commands.get(name, None)
