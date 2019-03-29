c4c93358f372a0e9c29a2889decca549bf0fb197
Roman <ronin911@gmail.com>
2019-03-30 00:04:42 +0200

    Table Formatter unittest

---
+++ weatherapp.core/weatherapp/core/tests/test_formatters.py
@@ -0,0 +1,50 @@
+import sys
+sys.path.insert(0, '..')
+
+import unittest
+
+from managers.formatters import TableFormatter, PlainText
+from app import App
+
+
+class TestTableFormatter(unittest.TestCase):
+    """ table formatter unit test """
+
+    def setUp(self):
+
+        self.table = TableFormatter()
+        self.test_weather_info = {"Temperature": 35}
+
+
+    def test_print_out(self):
+        """ print_out test """
+
+        printed_out = self.table.print_out(self.test_weather_info, 'Title')
+        printable = self.table._make_printable(self.test_weather_info)
+        nice_output = self.table._nice_output(printable, 'Title')
+
+        self.assertEqual(printed_out, nice_output)
+        self.assertIsInstance(printed_out, str)
+
+
+    def test_nice_output(self):
+        """ _nice_output test """
+
+        output_data = self.table._make_printable(self.test_weather_info)
+        nice_output = self.table._nice_output(output_data, 'Title')
+
+        # check return is str
+        self.assertIsInstance(nice_output, str)
+
+
+    def test_make_printable(self):
+        """ _make_printable test """
+
+        output_data = self.table._make_printable(self.test_weather_info)
+
+        # check return is list
+        self.assertIsInstance(output_data, list)
+
+
+if __name__ == "__main__":
+    unittest.main()
